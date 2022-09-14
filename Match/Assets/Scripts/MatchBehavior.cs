63d98e77e96f7bf97b493879f8e2bf210b4cf39c
Jaden <jadenmattinson6@gmail.com>
2022-09-14 13:24:26 -0600

    Challenge - Matching Game
    Finished UI animations for start button, restart button, and game over text. updated start button to also update score & high score. Updated Start button to update matcher's color, and re-enable sprite renderer updated noMatch to disable matcher's sprite renderer. Removed ResetBehavior, updated Reset button to function using SOs.

--- Match/Assets/Scripts/MatchBehavior.cs
+++ Match/Assets/Scripts/MatchBehavior.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,7 +6,7 @@ public class MatchBehavior : MonoBehaviour
 {
     public ID idObj;
     public UnityEvent matchEvent, noMatchEvent,noMatchDelayedEvent;
-
+    
     private IEnumerator OnTriggerEnter(Collider other)
     {
         var tempObj = other.GetComponent<IDContainerBehavior>();
